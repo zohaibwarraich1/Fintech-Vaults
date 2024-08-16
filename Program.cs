96a8c6ec00a07128f9be56ef0ad6088a831edabf
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2024-08-16 16:28:48 +0500

    Main Program File Of Code (C#)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fintech_Vaults.BusinessLayer;
+using Fintech_Vaults.PresentationLayer;
+using Fintech_Vaults.DataLayer;
+namespace Fintech_Vaults
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
